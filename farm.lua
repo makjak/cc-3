918e45e8d92265aa90f5ca316560dc9f25ba54b1
Steffen Haug <steffen@haug.rs>
2018-02-16 19:39:48 +0100

    refuel routine

--- farm.lua
+++ farm.lua
@@ -1,11 +1,5 @@
 
--- main loop
-while true do
-   break
-end
-
-
--- refuel routine
+-- refuel routine.
 function try_refuel()
    if turtle.getFuelLevel() < 512 then
       turtle.select(16)
@@ -15,7 +9,7 @@ function try_refuel()
    end
 end
 
--- move from center to forward/right corner
+-- move from center to forward/right corner.
 function move_to_corner()
    for i = 1, 4 do
       turtle.forward()
@@ -24,5 +18,34 @@ function move_to_corner()
    for i = 1, 4 do
       turtle.forward()
    end
-   
+end
+
+-- farm one tile.
+function farm_tile()
+   -- figure out what is under me.
+   local success, data = turtle.inspectDown()
+   if success and data.name == "minecraft:potatoes" then
+      -- if the block underneath me is potatoes:
+      if data.state.age == 7 then
+	 turtle.digDown()
+	 turtle.placeDown()
+      end
+   end
+end
+
+function farm_strip()
+   return
+end
+
+-- main loop
+while true do
+   -- wait 10 minutes between harvests.
+   os.sleep(600)
+
+   -- refuel and move to corner.
+   try_refuel()
+   move_to_corner()
+   turtle.turnRight()
+
+
 end
